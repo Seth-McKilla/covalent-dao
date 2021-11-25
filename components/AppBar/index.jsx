629500657e60ba6e625c78cd8bc49633a897ab01
Seth McCullough <seth@endevrs.dev>
2021-11-25 16:23:43 -0500

    Setup web3 connect button functionality

--- components/AppBar/index.jsx
+++ components/AppBar/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 // Mui
 import { default as MuiAppBar } from "@mui/material/AppBar";
@@ -7,8 +7,11 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
+// Hooks
+import { web3Connect } from "../../web3";
+
 export default function AppBar() {
-  const [modalOpen, setModalOpen] = useState(false);
+  //   const [modalOpen, setModalOpen] = useState(false);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -17,7 +20,12 @@ export default function AppBar() {
           <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
             DAOlytics
           </Typography>
-          <Button variant="contained" color="primary" size="large">
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={() => web3Connect()}
+          >
             Connect
           </Button>
         </Toolbar>
--- web3/index.js
+++ web3/index.js
@@ -1,15 +1,19 @@
 import Web3 from "web3";
 import Web3Modal from "web3modal";
 
-const providerOptions = {};
+export const web3Connect = async function () {
+  const providerOptions = {};
+  const web3Modal = new Web3Modal({
+    network: "mainnet",
+    cacheProvider: true,
+    providerOptions,
+  });
 
-const web3Modal = new Web3Modal({
-  network: "mainnet",
-  cacheProvider: true,
-  providerOptions,
-});
-
-const provider = await web3Modal.connect();
-
-const web3 = new Web3(provider);
-export default web3;
+  try {
+    const provider = await web3Modal.connect();
+    const web3 = new Web3(provider);
+    return web3;
+  } catch (error) {
+    console.log(error);
+  }
+};
